1526621ee9f6fb8069fdd909b66b820193a72a18
OOsipovs <olegs.osipovs@gmail.com>
2024-09-26 00:26:11 +0300

    SingIn page with functionality  added

--- fastdelivery/urls.py
+++ fastdelivery/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home),
+    path("sign_in/", auth_views.LoginView.as_view(template_name="sign_in.html")),
+    path("sign_out/", auth_views.LogoutView.as_view(next_page="/")),
     path("customer/", views.customer_page),
     path("bearer/", views.bearer_page),
 ]
